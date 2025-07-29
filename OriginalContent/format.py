ee18f23fe32503341614b9e5c4cc9bbffe8196ef
enenra <enenra9@gmail.com>
2025-07-29 19:44:20 +0200

    Update format.py

--- OriginalContent/format.py
+++ OriginalContent/format.py
@@ -3,40 +3,31 @@ import re
 
 def format_sbc(lines):
 
-    print("Before -------------------------------------------------")
-    print(lines)
-
     # XML indents
     result = re.sub(r"\s*(?=<\?xml)", "", lines, flags=re.MULTILINE)
 
     result = re.sub(r"\s*(?=<Definitions)", "", lines, flags=re.MULTILINE)
 
-    result = re.sub(r"\s*(?=<EntityComponents>)", "  ", lines, flags=re.MULTILINE)
-    result = re.sub(r"\s*(?=</EntityComponents>)", "  ", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=<EntityComponents>)", "\t", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=</EntityComponents>)", "\t", lines, flags=re.MULTILINE)
 
-    result = re.sub(r"\s*(?=<EntityComponent )", "    ", lines, flags=re.MULTILINE)
-    result = re.sub(r"\s*(?=</EntityComponent>)", "    ", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=<EntityComponent )", "\t\t", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=</EntityComponent>)", "\t\t", lines, flags=re.MULTILINE)
 
-    result = re.sub(r"\s*(?=<Id>)", "      ", lines, flags=re.MULTILINE)
-    result = re.sub(r"\s*(?=</Id>)", "      ", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=<Id>)", "\t\t\t", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=</Id>)", "\t\t\t", lines, flags=re.MULTILINE)
 
-    result = re.sub(r"\s*(?=<TypeId>)", "      ", lines, flags=re.MULTILINE)
-    result = re.sub(r"\s*(?=</TypeId>)", "      ", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=<TypeId>)", "\t\t\t\t", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=</TypeId>)", "\t\t\t\t", lines, flags=re.MULTILINE)
 
-    result = re.sub(r"\s*(?=<SubtypeId>)", "      ", lines, flags=re.MULTILINE)
-    result = re.sub(r"\s*(?=</SubtypeId>)", "      ", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=<SubtypeId>)", "\t\t\t\t", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=</SubtypeId>)", "\t\t\t\t", lines, flags=re.MULTILINE)
 
-    result = re.sub(r"\s*(?=<Description>)", "      ", lines, flags=re.MULTILINE)
-    result = re.sub(r"\s*(?=</Description>)", "      ", lines, flags=re.MULTILINE)
-
-    # XML formatting
-    #result = re.sub(r"</Description>\s*\n\s*</EntityComponent>", "</Description>\n    </EntityComponent>", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=<Description>)", "\t\t\t", lines, flags=re.MULTILINE)
+    result = re.sub(r"\s*(?=</Description>)", "\t\t\t", lines, flags=re.MULTILINE)
 
     # MES Tag formatting
-    result = re.sub(r"^\s*\[", "        [", result, flags=re.MULTILINE)
-
-    print("After -------------------------------------------------")
-    print(result)
+    result = re.sub(r"^\s*(?=\[)", "\t\t\t\t[", result)
 
     return result
 
@@ -44,7 +35,7 @@ def format_sbc(lines):
 def iterate_files(directory):
     for root, dirs, files in os.walk(directory):
         for filename in files:
-            if filename.endswith('.sbc') and filename.startswith("System_Disabled"):
+            if filename.endswith('.sbc'):
                 with open(os.path.join(root, filename), 'r+') as f:
                     result = format_sbc(f.read())
                     f.seek(0)
